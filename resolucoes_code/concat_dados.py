fbbbbf24c452c6fb4fd97f163546ae4abe249c55
Gabriel Campelo <154283961+CampeloGabriel1@users.noreply.github.com>
2025-09-22 01:15:48 +0000

    Resolução

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,7 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+concatenado = dado1 + dado2
+print("A string concatenada é:", concatenado)
+
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,6 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+texto = input("Digite um texto: ")
+vezes = int(input("Digite o número de repetições: "))
+resultado = texto * vezes
+print("Texto repetido:", resultado)
